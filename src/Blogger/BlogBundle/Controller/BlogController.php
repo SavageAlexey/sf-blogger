679821dbb37ecee183b59eb426f442f249a34faa
Aleksey Alieksieienko <nikolayevich.aleksey@isoftbet.com>
2018-07-19 18:09:51 +0300

    Blogger; Add show blog; Add fixtures

---
+++ src/Blogger/BlogBundle/Controller/BlogController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Blogger\blogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class BlogController extends Controller
+{
+    /**
+     * Show a blog entry
+     * @id int
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $blog = $em->getRepository('BloggerBlogBundle:Blog')->find($id);
+
+        if (!$blog) {
+            throw $this->createNotFoundException('Unable to find Blog post.');
+        }
+
+        return $this->render('BloggerBlogBundle:Blog:show.html.twig', array(
+            'blog'      => $blog,
+        ));
+    }
+}
