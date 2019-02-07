020296c382681cf6189c451cd924f374849b8662
Aleksey Alieksieienko <nikolayevich.aleksey@isoftbet.com>
2019-02-07 17:18:29 +0200

    BLOGGER; Fix the URL to make adding comment Unittest works;

--- src/Blogger/BlogBundle/Tests/Controller/BlogControllerTest.php
+++ src/Blogger/BlogBundle/Tests/Controller/BlogControllerTest.php
@@ -10,7 +10,7 @@ class BlogControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/1/a-day-with-symfony');
+        $crawler = $client->request('GET', '/blogger/show/16/a-day-with-symfony');
 
         $this->assertEquals(1, $crawler->filter('h2:contains("A day with Symfony2")')->count());
 
