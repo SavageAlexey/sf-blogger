c0f85746172619eff56a0cabfa12b785366279ea
Aleksey Alieksieienko <nikolayevich.aleksey@isoftbet.com>
2019-02-07 16:52:19 +0200

    BLOGGER; Implement sending email via swiftmailer; Implement testing sending email via Unittest;

--- src/Blogger/BlogBundle/Tests/Controller/PageControllerTest.php
+++ src/Blogger/BlogBundle/Tests/Controller/PageControllerTest.php
@@ -28,7 +28,31 @@ class PageControllerTest extends WebTestCase
         $blogTitle = $blogLink->text();
         $crawler = $client->click($blogLink->link());
 
-// Check the h2 has the blog title in it
-        $this->assertEquals(1, $crawler->filter('h2:contains("'. $blogTitle .'")')->count());
+        // Check the h2 has the blog title in it
+        $this->assertEquals(1, $crawler->filter('h2:contains("' . $blogTitle . '")')->count());
+    }
+
+    public function testContact()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/blogger/contact');
+
+        $this->assertEquals(1, $crawler->filter('h1:contains("Contact symblog")')->count());
+
+        // Select based on button value, or id or name for buttons
+        $form = $crawler->selectButton('Submit')->form();
+
+        $form['contact[name]']       = 'name';
+        $form['contact[email]']      = 'email@email.com';
+        $form['contact[subject]']    = 'Subject';
+        $form['contact[body]']       = 'The comment body must be at least 50 characters long as there is a validation constrain on the Enquiry entity';
+
+        $crawler = $client->submit($form);
+
+        // Need to follow redirect
+        $crawler = $client->followRedirect();
+
+        $this->assertEquals(1, $crawler->filter('.blogger-notice:contains("Your contact enquiry was successfully sent. Thank you!")')->count());
     }
 }
