49b73c7dc210f6ef2253cd746980054ae0ada5cc
Aleksey Alieksieienko <nikolayevich.aleksey@isoftbet.com>
2019-02-07 15:27:13 +0200

    BLOGGER; Add Unittest to check if articles are displayed, follow the detail link and check detail page;

--- src/Blogger/BlogBundle/Tests/Controller/PageControllerTest.php
+++ src/Blogger/BlogBundle/Tests/Controller/PageControllerTest.php
@@ -14,4 +14,21 @@ class PageControllerTest extends WebTestCase
 
         $this->assertEquals(1, $crawler->filter('h1:contains("About symblog")')->count());
     }
+
+    public function testIndex()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/blogger/');
+
+        // Check there are some blog entries on the page
+        $this->assertTrue($crawler->filter('article.blog')->count() > 0);
+
+        $blogLink = $crawler->filter('article.blog h2 a')->first();
+        $blogTitle = $blogLink->text();
+        $crawler = $client->click($blogLink->link());
+
+// Check the h2 has the blog title in it
+        $this->assertEquals(1, $crawler->filter('h2:contains("'. $blogTitle .'")')->count());
+    }
 }
